c01b7924770f6ae45205262697da169a79ddbc32
kintaro <118867955+Clarity69@users.noreply.github.com>
2025-10-09 20:27:02 +0800

    Add files via upload

---
+++ PBO/pelanggan.py
@@ -0,0 +1,29 @@
+# Template awal untuk Tugas 2
+class Pelanggan:
+    def __init__(self, nama, email):
+        self.nama = nama
+        self.email = email
+    # Implementasi: inisialisasi pelanggan dengan keranjang dan riwayat pesanan
+        pass
+    def buat_pesanan(self):
+    # Implementasi: buat pesanan dari keranjang
+        pass
+class Produk:
+    def __init__(self, nama, harga, stok):
+# Implementasi: inisialisasi produk
+        pass
+class Keranjang:
+    def __init__(self):
+    # Implementasi: inisialisasi keranjang kosong
+        pass
+def tambah_produk(self, produk, jumlah):
+    # Implementasi: tambahkan produk ke keranjang
+        pass
+class Pesanan:
+    def __init__(self, pelanggan):
+    # Implementasi: inisialisasi pesanan
+        pass
+class ItemPesanan:
+    def __init__(self, produk, jumlah):
+    # Implementasi: inisialisasi item pesanan
+        pass
